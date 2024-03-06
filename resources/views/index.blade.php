b23f2c4e1975b1950c1cf66c96081a1fdf610209
zackrspv <oldoak@gmail.com>
2024-03-06 09:05:45 -0700

    try to set radial value

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -652,6 +652,65 @@
 <!-- apexcharts -->
 <script src="{{ URL::asset('build/libs/apexcharts/apexcharts.min.js') }}"></script>
 
+<script>
+    var radialbarColors = getChartColorsArray("radialBar-chart");
+if (radialbarColors) {
+    var options = {
+        chart: {
+            height: 200,
+            type: 'radialBar',
+            offsetY: -10
+        },
+        plotOptions: {
+            radialBar: {
+                startAngle: -135,
+                endAngle: 135,
+                dataLabels: {
+                    name: {
+                        fontSize: '13px',
+                        color: undefined,
+                        offsetY: 60
+                    },
+                    value: {
+                        offsetY: 22,
+                        fontSize: '16px',
+                        color: undefined,
+                        formatter: function (val) {
+                            return val + "%";
+                        }
+                    }
+                }
+            }
+        },
+        colors: radialbarColors,
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shade: 'dark',
+                shadeIntensity: 0.15,
+                inverseColors: false,
+                opacityFrom: 1,
+                opacityTo: 1,
+                stops: [0, 50, 65, 91]
+            },
+        },
+        stroke: {
+            dashArray: 4,
+        },
+        series: [{{$progress}}],
+        labels: ['Progress'],
+
+    }
+
+    var chart = new ApexCharts(
+        document.querySelector("#radialBar-chart"),
+        options
+    );
+
+    chart.render();
+}
+</script>
+
 <!-- dashboard init -->
 <script src="{{ URL::asset('build/js/pages/dashboard.init.js') }}"></script>
 @endsection
