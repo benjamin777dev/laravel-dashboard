8c80b83d78321ee0eb19088733b0df421a014d4d
zackrspv <oldoak@gmail.com>
2024-03-06 08:13:08 -0700

    add missing js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 export default vite.defineConfig({
     build: {
         manifest: true,
-        // assetsDir: 'js',
         rtl: true,
         outDir: 'public/build/',
         cssCodeSplit: true,
@@ -31,6 +30,17 @@ export default vite.defineConfig({
                     'resources/scss/bootstrap.scss',
                     'resources/scss/icons.scss',
                     'resources/scss/app.scss',
+                    'resources/sass/app.scss',
+                    'resources/js/app.js',
+                    'resources/css/custom.css',
+                    'resources/js/custom.js',
+                    'resources/js/dashboard.js',
+                    'resources/css/dashboard.css',
+                    'resources/js/pipeline.js',
+                    'resources/css/pipeline.css',
+                    'resources/js/contacts.js',
+                    'resources/css/contacts.css',
+                    'resources/css/app.css'
                 ],
                 refresh: true,                
             }
