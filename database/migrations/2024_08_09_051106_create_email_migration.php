65c989c0c5cfba45a904887850023057c1cface2
Rajvant Chahal <chahal.rajvant.official@gmail.com>
2024-08-09 10:55:45 +0530

    update migration file

--- database/migrations/2024_08_09_051106_create_email_migration.php
+++ database/migrations/2024_08_09_051106_create_email_migration.php
@@ -4,29 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEmailsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('emails', function (Blueprint $table) {
-            $table->id(); 
-            $table->json('toEmail'); 
-            $table->json('ccEmail')->nullable(); 
-            $table->json('bccEmail')->nullable(); 
-            $table->string('fromEmail'); 
-            $table->string('subject')->nullable(); 
-            $table->longText('content')->nullable(); 
-            $table->unsignedBigInteger('userId'); 
-            $table->boolean('isEmailSent')->default(false); 
-            $table->boolean('isDeleted')->default(false); 
-            $table->string('sendEmailFrom')->nullable(); 
-            $table->string('message_id')->nullable(); 
-            $table->timestamps(); 
+            $table->id(); // Auto-incrementing ID field
+            $table->json('toEmail'); // Storing array of email IDs as JSON
+            $table->json('ccEmail')->nullable(); // Optional array of CC email IDs as JSON
+            $table->json('bccEmail')->nullable(); // Optional array of BCC email IDs as JSON
+            $table->string('fromEmail'); // String for the sender's email
+            $table->string('subject')->nullable(); // Email subject, optional
+            $table->longText('content')->nullable(); // Email content, optional
+            $table->unsignedBigInteger('userId'); // Reference to the user who sent the email
+            $table->boolean('isEmailSent')->default(false); // Boolean to check if the email was sent
+            $table->boolean('isDeleted')->default(false); // Boolean to check if the email was deleted
+            $table->string('sendEmailFrom')->nullable(); // Service used to send the email, optional
+            $table->string('message_id')->nullable(); // Message ID from the email service, optional
+            $table->timestamps(); // Timestamps for created_at and updated_at
 
             // Foreign key constraint (optional)
             $table->foreign('userId')->references('id')->on('users')->onDelete('cascade');
@@ -35,11 +33,9 @@ class CreateEmailsTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('emails');
     }
-}
+};
