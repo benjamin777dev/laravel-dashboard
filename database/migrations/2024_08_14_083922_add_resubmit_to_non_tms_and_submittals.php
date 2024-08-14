413aca1fe4e86ff0c94bbb99c863f5685a8a07dc
zackrspv <oldoak@gmail.com>
2024-08-14 08:46:43 -0600

    rename and use generic class

--- database/migrations/2024_08_14_083922_add_resubmit_to_non_tms_and_submittals.php
+++ database/migrations/2024_08_14_083922_add_resubmit_to_non_tms_and_submittals.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddResubmitFieldsToNonTmsAndSubmittals extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -41,4 +41,4 @@ class AddResubmitFieldsToNonTmsAndSubmittals extends Migration
             $table->dropColumn('resubmitting_why_list_all_changes');
         });
     }
-}
+};
