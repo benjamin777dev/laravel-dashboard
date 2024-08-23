6ab86f2862d17f0a1a2750b9f7dba1db4a600066
zackrspv <oldoak@gmail.com>
2024-08-23 13:12:10 -0600

    dont log sensitive information

--- app/Models/User.php
+++ app/Models/User.php
@@ -105,7 +105,6 @@ class User extends Authenticatable
         if ($this->isAccessTokenValid()) {
             Log::info('getAccessToken: valid');
             $decryptAccessToken = Crypt::decryptString($this->access_token);
-            Log::info('Decrypt Access Token' . $decryptAccessToken);
             return $decryptAccessToken;
         }
 
@@ -137,8 +136,6 @@ class User extends Authenticatable
             'grant_type' => 'refresh_token',
         ];
 
-        Log::info("Params: " . print_r($curlParams, true));
-
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://accounts.zoho.com/oauth/v2/token',
             CURLOPT_RETURNTRANSFER => true,
@@ -151,17 +148,13 @@ class User extends Authenticatable
             CURLOPT_POSTFIELDS => $curlParams));
 
         $response = curl_exec($curl);
-        Log::info("Response: " . print_r($response, true));
         curl_close($curl);
         $responseBody = json_decode($response);
-        Log::info("Response body: " . print_r($responseBody, true));
 
         if (isset($responseBody->access_token)) {
-            Log::info("Response body: " . print_r($responseBody, true));
             $this->access_token = Crypt::encryptString($responseBody->access_token);
             $this->token_expires_at = now()->addSeconds($responseBody->expires_in);
             $this->save();
-            Log::info("Access token: " . $this->access_token);
             return $responseBody->access_token;
         } else {
             Log::error("Error refreshing access token: " . $response);
