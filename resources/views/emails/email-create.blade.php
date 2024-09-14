8c68b60769404fff911362aafd63cca24133f565
kamillm <kamillm.ponce@gmail.com>
2024-09-14 13:40:38 -0300

    remove bootstrap icon module

--- resources/views/emails/email-create.blade.php
+++ resources/views/emails/email-create.blade.php
@@ -198,9 +198,7 @@
             statusbar: false,
             setup: function(editor) {
                 editor.ui.registry.addIcon('recordIcon', 
-                    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-record-circle" viewBox="0 0 16 16">' + 
-                    '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>' +
-                    '<path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/></svg>'
+                    '<i class="mdi mdi-record-circle-outline fs-3"></i>'
                 );
                 editor.ui.registry.addButton('customSelect', {
                     icon: 'gallery',
@@ -290,9 +288,9 @@
                                                                 <button class="btn" type="button" id="startRecordButton"
                                                                     data-bs-toggle="tooltip" data-bs-placement="top"
                                                                     data-bs-title="This top tooltip is themed via CSS variables.">
-                                                                    <i class="bi bi-play-circle"></i>
+                                                                    <i class="mdi mdi-play-circle-outline"></i>
                                                                 </button>
-                                                                <button type="button" id="stopRecordButton"><i class="bi bi-stop-circle"></i></button>
+                                                                <button type="button" id="stopRecordButton"><i class="mdi mdi-stop-circle-outline"></i></button>
                                                             </div>
                                                         </div>
                                                     </div>
